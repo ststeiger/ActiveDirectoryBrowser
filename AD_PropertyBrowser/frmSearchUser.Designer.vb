1499ff15bc2fe8556488d3bd5293c081025a1589
ststeiger <quandary82@hailmail.net>
2015-01-26 13:03:11 +0100

    Added files to git

---
+++ AD_PropertyBrowser/frmSearchUser.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSearchUser
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSearchUser))
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnAuth = New System.Windows.Forms.Button()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.dgvDisplayData = New System.Windows.Forms.DataGridView()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        CType(Me.dgvDisplayData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(359, 12)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(260, 20)
+        Me.txtPassword.TabIndex = 9
+        '
+        'btnAuth
+        '
+        Me.btnAuth.Location = New System.Drawing.Point(625, 11)
+        Me.btnAuth.Name = "btnAuth"
+        Me.btnAuth.Size = New System.Drawing.Size(75, 23)
+        Me.btnAuth.TabIndex = 8
+        Me.btnAuth.Text = "Auth"
+        Me.btnAuth.UseVisualStyleBackColor = True
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(12, 13)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(260, 20)
+        Me.txtUserName.TabIndex = 7
+        '
+        'dgvDisplayData
+        '
+        Me.dgvDisplayData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvDisplayData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDisplayData.Location = New System.Drawing.Point(12, 40)
+        Me.dgvDisplayData.Name = "dgvDisplayData"
+        Me.dgvDisplayData.Size = New System.Drawing.Size(872, 522)
+        Me.dgvDisplayData.TabIndex = 6
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(278, 11)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 5
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'frmSearchUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(896, 573)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.btnAuth)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.dgvDisplayData)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmSearchUser"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmSearchUser"
+        CType(Me.dgvDisplayData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents txtPassword As System.Windows.Forms.TextBox
+    Private WithEvents btnAuth As System.Windows.Forms.Button
+    Private WithEvents txtUserName As System.Windows.Forms.TextBox
+    Private WithEvents dgvDisplayData As System.Windows.Forms.DataGridView
+    Private WithEvents btnSearch As System.Windows.Forms.Button
+End Class
---
+++ AD_PropertyBrowser/frmSearchUser.vb
@@ -0,0 +1,182 @@
+﻿Public Class frmSearchUser
+
+    Protected m_RootDn As String
+
+
+    Public Sub New()
+        Me.New(Nothing)
+    End Sub ' End Constructor frmSearchUser
+
+
+    Public Sub New(RootDn As String)
+        m_RootDn = RootDn
+        InitializeComponent()
+    End Sub ' End Constructor frmSearchUser
+
+
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim strUserName As String = Me.txtUserName.Text
+        Me.dgvDisplayData.DataSource = GetUserList(strUserName).DefaultView
+
+        Me.dgvDisplayData.Sort(Me.dgvDisplayData.Columns(Me.dgvDisplayData.Columns(0).Name), System.ComponentModel.ListSortDirection.Ascending)
+    End Sub
+
+
+
+    Private Function GetUserList(strUserName As String) As System.Data.DataTable
+        Dim dt As System.Data.DataTable = New DataTable()
+
+        dt.Columns.Add("sAMAccountName", GetType(String))
+        dt.Columns.Add("DistinguishedName", GetType(String))
+        dt.Columns.Add("cn", GetType(String))
+        dt.Columns.Add("DisplayName", GetType(String))
+
+        dt.Columns.Add("EmailAddress", GetType(String))
+        dt.Columns.Add("DomainName", GetType(String))
+        dt.Columns.Add("Department", GetType(String))
+        dt.Columns.Add("title", GetType(String))
+        dt.Columns.Add("company", GetType(String))
+        dt.Columns.Add("memberof", GetType(String))
+
+
+        'using (DirectoryEntry rootDSE = new DirectoryEntry("LDAP://DC=cor,DC=local", username, password))
+        Using rootDSE As System.DirectoryServices.DirectoryEntry = LdapTools.GetDE(m_RootDn)
+
+            Using search As New System.DirectoryServices.DirectorySearcher(rootDSE)
+                search.PageSize = 1001
+                ' To Pull up more than 100 records.
+                'search.Filter = "(&(objectClass=user)(!userAccountControl:1.2.840.113556.1.4.803:=2))";//UserAccountControl will only Include Non-Disabled Users.
+
+                Dim strUserCondition As String = ""
+                If Not String.IsNullOrEmpty(strUserName) Then
+                    strUserCondition = (Convert.ToString("(samAccountName=") & strUserName) + ")"
+                End If
+
+                'UserAccountControl will only Include Non-Disabled Users.
+                'search.Filter = "(&(objectClass=user)(!userAccountControl:1.2.840.113556.1.4.803:=2)(samAccountName=stefan.steiger))";
+
+                search.Filter = String.Format("(&(objectClass=user)(!userAccountControl:1.2.840.113556.1.4.803:=2){0})", strUserCondition)
+
+                Using result As System.DirectoryServices.SearchResultCollection = search.FindAll()
+
+                    For Each item As System.DirectoryServices.SearchResult In result
+                        Dim sAMAccountName As String = Nothing
+                        Dim DistinguishedName As String = Nothing
+                        Dim cn As String = Nothing
+                        Dim DisplayName As String = Nothing
+                        Dim EmailAddress As String = Nothing
+                        Dim DomainName As String = Nothing
+                        Dim Department As String = Nothing
+                        Dim title As String = Nothing
+                        Dim company As String = Nothing
+                        Dim memberof As String = Nothing
+
+
+                        If item.Properties("sAMAccountName").Count > 0 Then
+                            sAMAccountName = item.Properties("sAMAccountName")(0).ToString()
+                        End If
+
+
+                        If item.Properties("distinguishedName").Count > 0 Then
+                            DistinguishedName = item.Properties("distinguishedName")(0).ToString()
+                        End If
+
+
+                        If item.Properties("cn").Count > 0 Then
+                            cn = item.Properties("cn")(0).ToString()
+                        End If
+
+                        If item.Properties("DisplayName").Count > 0 Then
+                            DisplayName = item.Properties("DisplayName")(0).ToString()
+                        End If
+
+
+                        If item.Properties("mail").Count > 0 Then
+                            EmailAddress = item.Properties("mail")(0).ToString()
+                        End If
+                        If item.Properties("SamAccountName").Count > 0 Then
+                            DomainName = item.Properties("SamAccountName")(0).ToString()
+                        End If
+                        If item.Properties("department").Count > 0 Then
+                            Department = item.Properties("department")(0).ToString()
+                        End If
+                        If item.Properties("title").Count > 0 Then
+                            title = item.Properties("title")(0).ToString()
+                        End If
+                        If item.Properties("company").Count > 0 Then
+                            company = item.Properties("company")(0).ToString()
+                        End If
+
+                        If item.Properties("DistinguishedName").Count > 0 Then
+                            DistinguishedName = item.Properties("DistinguishedName")(0).ToString()
+                        End If
+
+                        If item.Properties("memberof").Count > 0 Then
+                            ' memberof = item.Properties["memberof"][0].ToString();
+                            memberof = LdapTools.GetGroups(DistinguishedName, True)
+                        End If
+
+
+                        If item.Properties("AccountExpirationDate").Count > 0 Then
+                            Dim aaa As String = item.Properties("AccountExpirationDate")(0).ToString()
+                        End If
+
+
+                        Dim dr As System.Data.DataRow = dt.NewRow()
+
+                        dr("sAMAccountName") = sAMAccountName
+                        dr("DistinguishedName") = DistinguishedName
+                        dr("cn") = cn
+                        dr("DisplayName") = DisplayName
+                        dr("EmailAddress") = EmailAddress
+                        dr("DomainName") = DomainName
+                        dr("Department") = Department
+                        dr("title") = title
+                        dr("company") = company
+                        dr("memberof") = memberof
+
+                        dt.Rows.Add(dr)
+
+
+
+                        DisplayName = String.Empty
+                        EmailAddress = String.Empty
+                        DomainName = String.Empty
+                        Department = String.Empty
+                        title = String.Empty
+                        company = String.Empty
+
+                        'rootDSE.Dispose();
+                        memberof = String.Empty
+                    Next item
+
+                End Using ' SearchResultCollection result 
+
+            End Using ' search 
+
+        End Using ' rootDSE
+
+        Return dt
+    End Function ' GetUserList
+
+
+    Private Function GetUserList() As System.Data.DataTable
+        Return GetUserList(Nothing)
+    End Function ' GetUserList
+
+
+    Private Sub btnAuth_Click(sender As Object, e As EventArgs) Handles btnAuth.Click
+        '    Dim usr As String = Me.txtUserName.Text
+        '    Dim password As String = Me.txtPassword.Text
+
+        '    MsgBox(LdapTools.IsAuthenticated(Me.m_RootDn, usr, password))
+    End Sub ' btnAuth_Click 
+
+
+    Public Sub MsgBox(obj As Object)
+        System.Windows.Forms.MessageBox.Show(System.Convert.ToString(obj))
+    End Sub ' MsgBox
+
+
+End Class
